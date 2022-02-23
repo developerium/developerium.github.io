5db3aa1a9ebab1cd71e037d72f1c30efb9a3171a
developerium <kheradmand.v1@gmail.com>
2022-02-23 10:17:33 +0100

    Create a couple of objects and cast shadows

--- src/DiceWrapper.tsx
+++ src/DiceWrapper.tsx
@@ -11,6 +11,25 @@ const Orbit = () => {
   return <orbitControls args={[camera, gl.domElement]} />;
 };
 
+const Floor = () => {
+  return (
+    <mesh receiveShadow>
+      <boxBufferGeometry args={[20, 0.1, 20]} />
+      <meshPhysicalMaterial color={'green'} />
+    </mesh>
+  );
+};
+
+const LightBulb = () => {
+  return (
+    <mesh position={[0, 7, 0]}>
+      <pointLight color="white" intensity={2} castShadow />
+      <sphereBufferGeometry args={[0.5]} />
+      <meshPhongMaterial emissive="yellow" />
+    </mesh>
+  );
+};
+
 function Box(props: JSX.IntrinsicElements['mesh']) {
   const ref = useRef<THREE.Mesh>(null);
   const [hovered, hover] = useState(false);
@@ -20,8 +39,7 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
     if (!ref.current) return;
 
     ref.current.rotation.x += 0.01;
-
-    // ref.current.position.x += 0.01;
+    ref.current.rotation.y += 0.01;
   });
 
   return (
@@ -32,24 +50,33 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
+      castShadow
+      receiveShadow
     >
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <boxBufferGeometry args={[2, 2, 2]} />
+      <meshPhysicalMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   );
 }
 
 export const DiceWrapper = () => {
   return (
-    <Canvas camera={{ position: [3, 3, 8] }}>
-      <ambientLight intensity={0.2} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <Box position={[-5, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    <Canvas
+      camera={{ position: [3, 5, 20], fov: 60 }}
+      style={{ backgroundColor: 'black' }}
+      shadows
+    >
+      <Floor />
 
-      <mesh position={[5, 0, 0]}>
-        <sphereBufferGeometry />
-        <meshStandardMaterial color="hotpink" />
+      <ambientLight intensity={0.2} />
+      <LightBulb />
+
+      <Box position={[-5, 2, 0]} />
+      <Box position={[1.2, 4, 0]} />
+
+      <mesh position={[5, 2, 4]} castShadow>
+        <sphereBufferGeometry args={[0.5]} />
+        <meshPhysicalMaterial color="hotpink" />
       </mesh>
 
       <axesHelper scale={5} />
