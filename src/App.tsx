0f3148a8dbf83b4dfdd8eb4cb76c3b301ff3241d
developerium <vahid@kheradmand.de>
2023-12-10 22:21:47 +0100

    Log the app version and remove more unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { ThemeProvider } from '@mui/material'
 import { InfoCard } from './components/InfoCard'
@@ -6,8 +6,15 @@ import myImage from './assets/me-01.jpg'
 import { Layout } from './components/Layout'
 import { appTheme } from './config/theme-config'
 import { SocialLinks } from './components/SocialLinks'
+import packageInfo from '../package.json'
+
+const { version } = packageInfo
 
 function App() {
+  useEffect(() => {
+    console.log(`app version: ${version}`)
+  }, [])
+
   return (
     <ThemeProvider theme={appTheme}>
       <Layout>
