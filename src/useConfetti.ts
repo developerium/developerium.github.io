768509dd64f98046e56993c4ab7d95be9eb4ce98
developerium <vahid@kheradmand.de>
2023-12-12 23:17:19 +0100

    Set 2 variation of confetti

--- src/useConfetti.ts
+++ src/useConfetti.ts
@@ -1,21 +1,34 @@
 import party from 'party-js'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 interface UseConfettiProps {
   targetId: string
 }
 
 export const useConfetti = ({ targetId }: UseConfettiProps) => {
+  const [variant, setVariant] = useState(false)
+
   const throwConfetti = useCallback(() => {
     const source = document.getElementById(targetId)
-    if (source) {
+    if (!source) {
+      console.log('Target source for confetti not found!')
+      return
+    }
+
+    if (variant) {
       party.confetti(source, {
-        count: party.variation.range(50, 100),
+        count: party.variation.range(0, 100),
+        size: party.variation.range(0.6, 1.4),
       })
     } else {
-      console.log('Target source for confetti not found!')
+      party.sparkles(source, {
+        count: party.variation.range(10, 60),
+        speed: party.variation.range(50, 300),
+      })
     }
-  }, [])
+
+    setVariant(!variant)
+  }, [variant, setVariant])
 
   return {
     throwConfetti,
